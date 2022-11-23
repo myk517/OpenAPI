13f626aee71dc060f8bd8cb9ff5c7fa915b03023
myk <koreash321@gmail.com>
2022-11-23 23:10:26 +0900

    토큰발행 수정, 입금이체 수정

--- src/api/auth/AuthPage.js
+++ src/api/auth/AuthPage.js
@@ -2,67 +2,75 @@ import React, {useEffect, useState } from "react";
 import axios from "axios";
 
 function AuthPage(){
-    const [access_token, setAccess_token] = useState();
+    const [accessTokenOob, setAccessTokenOob] = useState();
 
-    const onSubmit = () =>{
-        window.location.href = 
-         "https://testapi.openbanking.or.kr/oauth/2.0/authorize?"
-        +"response_type=code&"
-        +"client_id=9815d321-e0e1-462c-ad06-9cfa5d46c53c&"
-        +"redirect_uri=http://localhost:3000/auth/success&"
-        +"scope=login inquiry transfer&"
-        +"state=b80BLsfigm9OokPTjy03elbJqRHOfGSY&"
-        +"auth_type=0";
-        // redirect url 이 호출되면 해당 code 값을 기반으로 token을 발급한다.
-        
-        // axios.get("http://localhost:8080/api/v1/oauth/getOauth")
-        // .then((res)=>{console.log('success...', 
+    const onSubmit = () => {
+      window.location.href =
+        "https://testapi.openbanking.or.kr/oauth/2.0/authorize?" +
+        "response_type=code&" +
+        "client_id=9815d321-e0e1-462c-ad06-9cfa5d46c53c&" +
+        "redirect_uri=http://localhost:3000/auth/success&" +
+        "scope=login inquiry transfer&" +
+        "state=b80BLsfigm9OokPTjy03elbJqRHOfGSY&" +
+        "auth_type=0";
+      // redirect url 이 호출되면 해당 code 값을 기반으로 token을 발급한다.
 
-        // res)})
-        // .catch((res)=>{console.log('fail...', res)});
+      // axios.get("http://localhost:8080/api/v1/oauth/getOauth")
+      // .then((res)=>{console.log('success...',
 
-        }
+      // res)})
+      // .catch((res)=>{console.log('fail...', res)});
+    };
 
-        const getOobToken = ()=>{
-          let datas =  {
-            params : 
-            {"client_id" : "9815d321-e0e1-462c-ad06-9cfa5d46c53c",
-              "client_secret":"39831a41-c3d7-47f2-b72f-bc82c4ca5e53",
-              "scope":"oob",
-              "grant_type":"client_credentials"}
-          }
-          axios.post("http://localhost:8080/api/v1/token/getToken", null,datas)
-          .then(()=>{console.log('getOobToken Success...')})
-          .catch(()=>{console.log('getOobToken fail...')});
-        }
+    const getOobToken = () => {
+      let datas = {
+        params: { client_id: "9815d321-e0e1-462c-ad06-9cfa5d46c53c", client_secret: "39831a41-c3d7-47f2-b72f-bc82c4ca5e53", scope: "oob", grant_type: "client_credentials" },
+      };
+      axios
+        .post("http://localhost:8080/api/v1/token/getToken", null, datas)
+        .then((res) => {
+          console.log("getOobToken Success...", res);
+          setAccessTokenOob(res.data.access_token);
+          localStorage.setItem("accessTokenOob", accessTokenOob);
+        })
+        .catch((res) => {
+          console.log("getOobToken fail...", res);
+        });
+    };
 
-        const getToken = () =>{
-            console.log('asdg');
-            let data = {
-              "code": "PKY41xhBOrWF2N110lzFveiKjJmij1",
-              "client_id": "9815d321-e0e1-462c-ad06-9cfa5d46c53c",
-              "client_secret": "39831a41-c3d7-47f2-b72f-bc82c4ca5e53",
-              "redirect_uri": "http://localhost:3000",
-              "grant_type": "authorization_code"
-              };
-            axios.post("http://localhost:8080/api/v1/getToken", data).
-            then((res)=>{
-              console.log('success..', res); 
-              setAccess_token(res.data.access_token);
-              sessionStorage.setItem("access_token", access_token);
-            }).
-            catch((res)=>{console.log('fail...', res)});
-          }
+    const getSe = () => {
+      console.log("asdfasdfasdf", localStorage.getItem("accessTokenOob"));
+    };
 
-    return(
-        <>
+    return (
+      <>
         <h2> Auth Page ...</h2>
-
-        <button onClick={()=>{onSubmit()}}>인증하기123 (/authorize)</button>
-        <button onClick={()=>{getOobToken()}}>oob 토큰 발급 받기 (/authorize)</button>
+        <button
+          onClick={() => {
+            onSubmit();
+          }}
+        >
+          인증하기123 (/authorize)
+        </button>
+        <button
+          onClick={() => {
+            getOobToken();
+          }}
+        >
+          oob 토큰 발급 받기 (/authorize)
+        </button>
+        <br />
+        oob 토큰 :{accessTokenOob} <br />
+        <button
+          onClick={() => {
+            getSe();
+          }}
+        >
+          localStorage.getItme(""){" "}
+        </button>
         {/* <button onClick={()=>{getToken()}}>토큰발급123 (/token)</button> */}
-        </>
-    )
+      </>
+    );
 }
 
 export default AuthPage;
--- src/api/transfer/Transferpage.js
+++ src/api/transfer/Transferpage.js
@@ -2,33 +2,95 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Transfer = ()=>{
+  //const [accessTokenOob, setAccessTokenOob] = useState();
+  const accessTokenOob = localStorage.getItem("accessTokenOob");
 
-    const withdrawApi =()=>{
-        console.log('Run withdraw Funcion ...');
-        axios.post("http://localhost:8080/api/v1/transfer/withdraw", null, {params: {
-            "bank_tran_id" : "value",
-            "cntr_account_type" : "value",
-            "cntr_account_num" : "value",
-            "dps_print_content" : "value",
-            "fintech_use_num" : "value",
-            "wd_print_content" : "value",
-            "tran_amt" : "value",
-            "tran_dtime" : "value",
-            "req_client_name" : "value",
-            "req_client_account_num" : "value",
-            "req_client_num" : "value",
-            "transfer_purpose" : "value"
-        }})
-        .then((res)=>{console.log('success...', res)})
-        .catch((res) => {console.log('fail... ', res)});
-    }
+  const withdrawApi = () => {
+    console.log("Run withdraw Funcion ...");
+    console.log("oob token>> ", accessTokenOob);
+    axios
+      .post("http://localhost:8080/api/v1/transfer/withdraw", null, {
+        params: {
+          bank_tran_id: "value",
+          cntr_account_type: "value",
+          cntr_account_num: "value",
+          dps_print_content: "value",
+          fintech_use_num: "value",
+          wd_print_content: "value",
+          tran_amt: "value",
+          tran_dtime: "value",
+          req_client_name: "value",
+          req_client_account_num: "value",
+          req_client_num: "value",
+          transfer_purpose: "value",
+        },
+      })
+      .then((res) => {
+        console.log("success...", res);
+      })
+      .catch((res) => {
+        console.log("fail... ", res);
+      });
+  };
 
-    return(
-        <>
-        <h2>Transfer Page...</h2> <br/>
-        <button onClick={()=>{withdrawApi()}}> 출금이체 </button> <br/>
-        <button> 입금이체 </button>
-        </>
-    );
+  const depositApi = () => {
+    console.log("depositApi Function...");
+    console.log("oob token>> ", accessTokenOob);
+    let datas = {
+      access_token: accessTokenOob,
+      cntr_account_type: "N",
+      cntr_account_num: "100000000001",
+      wd_pass_phrase: "NONE",
+      wd_print_content: "환불금액",
+      name_check_option: "off",
+      tran_dtime: "20201001150133",
+      req_cnt: "1",
+      tran_no: "1",
+      bank_tran_id: "M202201963U16134605A",
+      bank_code_std: "004",
+      account_num: "61250201399911",
+      account_holder_name: "파리바게트",
+      print_content: "빵결제",
+      tran_amt: "000000004800",
+      req_client_name: "홍길동",
+      req_client_bank_code: "097",
+      req_client_account_num: "00012300000678",
+      req_client_num: "HONGGILDONG1234",
+      transfer_purpose: "TR",
+    };
+    axios
+      .post("http://localhost:8080/api/v1/transfer/deposit/acnt_num", datas)
+      .then((res) => {
+        console.log("success...", res);
+      })
+      .catch((res) => {
+        console.log("fail...", res);
+      });
+  };
+
+  return (
+    <>
+      <h2>Transfer Page...</h2> <br />
+      <button
+        onClick={() => {
+          withdrawApi();
+        }}
+      >
+        {" "}
+        출금이체{" "}
+      </button>{" "}
+      <br />
+      <button
+        onClick={() => {
+          depositApi();
+        }}
+      >
+        {" "}
+        입금이체{" "}
+      </button>
+      <br />
+      oob >>>> {accessTokenOob}
+    </>
+  );
 }
 export default Transfer;
